873b62ca326d26291880c7f6af33a03350b6f5e5
Caven Fernanda <fernanda17caven@gmail.com>
2021-04-07 13:15:14 +0200

    Programme is now able to select the row in order to manipulate data

--- src/Gui/TablePanel.java
+++ src/Gui/TablePanel.java
@@ -4,6 +4,8 @@ import Model.Person;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -28,9 +30,22 @@ public class TablePanel extends JPanel {
         table.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+
+                int row = table.rowAtPoint(e.getPoint()); //gives me the table row
+                table.getSelectionModel().setSelectionInterval(row,row); //selecting the row that was clicked
+
+                System.out.println(row);
                 if(e.getButton() == MouseEvent.BUTTON3){
                     popupMenu.show(table, e.getX(),e.getY());
                 }
+            }
+        });
+
+        removeItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = table.getSelectedRow();
+
             }
         });
 
